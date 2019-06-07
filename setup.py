3ac051738f54f721f6c27dbb2bc87bbe579bf722
zexin <chenzexin@sjtu.edu.cn>
2019-06-07 19:10:42 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='torchsample',
+      version='0.1.3',
+      description='High-Level Training, Augmentation, and Sampling for Pytorch',
+      author='Zexin Chen',
+      author_email='czxsjtu@gmail.com',
+      packages=find_packages()
+     )
